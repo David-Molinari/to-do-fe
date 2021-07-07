99cab52d7539f2cecf5c7626fa93b2f974ac355f
David Molinari <david.m.molinari@gmail.com>
2021-07-07 01:02:58 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Auth from './components/Auth';
+import ToDo from './components/ToDo';
 
 function App() {
+
+  // check for token to see if logged in
+  // if so, send to page; if not, send to auth
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
